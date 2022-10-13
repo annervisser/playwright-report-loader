0193c2f17f5f8b02fb8fef6dae93dd39ff29e687
Anner Visser <a.h.c.visser@scienta.nl>
2022-10-13 11:29:51 +0200

    switch to https for playwright checkout

--- download-and-patch-trace-viewer.sh
+++ download-and-patch-trace-viewer.sh
@@ -6,7 +6,7 @@ cd "$MY_DIR"
 
 TEMPD=$(mktemp -d)
 cd "$TEMPD"
-git clone --depth 1 git@github.com:microsoft/playwright.git
+git clone --depth 1 https://github.com/microsoft/playwright.git
 cd playwright
 git apply "$MY_DIR/patches/allow-blob-urls-in-trace-viewer.patch"
 npm ci
